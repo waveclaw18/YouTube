aec7a83746690ab111ae97950f85954cd56e2030
Waveclaw18 <67701009+waveclaw18@users.noreply.github.com>
2020-09-16 12:47:35 -0700

    Update MainCode.js

--- MainCode.js
+++ MainCode.js
@@ -1,1 +1,18 @@
+const Discord = require('discord.js')
+const client = new Discord.Client
 
+client.once('ready', () => {
+  console.log('Bot is online!')
+})
+
+client.on('message', async (message) => {
+  const args = message.content.slice(prefix.length).split(/ +/);
+  const command = args.shift().toLowerCase();
+  
+  if(command === 'ping'){
+    message.channel.send('Pong!')
+  }
+})
+
+
+client.login('your-bot-token')
